6a84cc1eae9c9bfecdc22d912bfcb845d256b997
Christian Jácome <christian.jacome.mora@udla.edu.ec>
2026-01-24 21:09:57 -0500

    refactor: add getTaskById mock to studentServiceMock with TaskServiceDTO

--- src/__mocks__/studentServiceMock.ts
+++ src/__mocks__/studentServiceMock.ts
@@ -1,4 +1,5 @@
 import type {StudentServiceDTO} from "../models/dtos/StudentServiceDTO.ts";
+import type {TaskServiceDTO} from "../models/dtos/TaskServiceDTO.ts";
 
 const studentServiceMock = {
     getStudentMe: async (): Promise<StudentServiceDTO> => {
@@ -14,6 +15,20 @@ const studentServiceMock = {
                 id: "9acec5e6-845b-410f-aef6-b9f9ad2f7fa0",
                 label: "2026-01"
             }
+        }
+    },
+
+    getTaskById: async (): Promise<TaskServiceDTO> => {
+        return {
+            id: "e10e8a9d-b2fe-4d9c-bcdd-6207c53d95f3",
+            title: "Tarea de prueba",
+            summary: "Descripción...",
+            deadline: "2026-02-01T18:00:00",
+            minScoreToPass: 0.7,
+            status: "NOT_STARTED",
+            currentAttempt: 1,
+            maxAttempts: 11,
+            remainingAttempts: 10
         }
     }
 }
